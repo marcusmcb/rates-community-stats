ec68474eebf8a8a920b33871ff03b1af82952227
marcusmcb <marcusmcb@gmail.com>
2025-01-30 11:03:55 -0800

    fixed typo

--- src/components/LeftPanel.js
+++ src/components/LeftPanel.js
@@ -26,7 +26,8 @@ const LeftPanel = ({
 				onChange={(e) => onPlaylistChange(e.target.value)}
 				className='left-panel-selector'
 			>
-				<option value='December'>December 2024 Playlist</option>								
+				<option value='January'>January 2025 Playlist</option>
+				<option value='December'>December 2024 Playlist</option>
 				<option value='November'>November 2024 Playlist</option>
 				<option value='October'>October 2024 Playlist</option>
 				<option value='September'>September 2024 Playlist</option>
@@ -37,7 +38,6 @@ const LeftPanel = ({
 				<option value='April'>April 2024 Playlist</option>
 				<option value='March'>March 2024 Playlist</option>
 				<option value='February'>February 2024 Playlist</option>
-				<option value='January'>January 2024 Playlist</option>				
 			</select>
 			<input
 				type='text'
